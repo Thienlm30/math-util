50ad2019fcee39239d0d91b714007f53d09ceda0
Thienlm30 <lmthien.30@gmail.com>
2023-12-15 15:30:03 +0700

    lan-thu-hai-up-code

--- src/com/fu/mathutil/MathUtil.java
+++ src/com/fu/mathutil/MathUtil.java
@@ -13,6 +13,10 @@ public class MathUtil {
         System.out.println("expected 6! = 720; actual: " + MathUtilLive.getFactorial(6));
         System.out.println("expected 0! = 1; actual: " + MathUtilLive.getFactorial(0));
         
+        // thêm code sau lần đầu tiên up code lên server 15:27 15/12/2023
+        System.out.println("expected 1! = 1; actual: " + MathUtilLive.getFactorial(1));
+        System.out.println("expected 3! = 6; actual: " + MathUtilLive.getFactorial(3));
+        
         // kĩ thuật kiểm thử phầm mền: ước lượng xem giá trị trả về của hàm là gì
         // expected value: 5! = 120
         // hàm ai viết khi chạy thực tế là mấy - actual, giả sử 120
